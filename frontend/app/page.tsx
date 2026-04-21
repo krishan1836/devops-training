52019775c490332663207b189dc6a5ab20bb8576
krishan1836 <krishan.mishra@oakter.com>
2026-04-21 15:56:14 +0530

    fix: remove server code, make full client component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,21 +1,3 @@
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-async function getData() {
-  try {
-    const baseUrl =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
-
-    const res = await fetch(`${baseUrl}/api/...`, {
-      cache: "no-store",
-});
-    return await res.json();
-  } catch (error) {
-    console.log("API error:", error);
-    return { status: "API not reachable" }; // fallback
-  }
-}
-
 "use client";
 
 import { useEffect, useState } from "react";
@@ -24,7 +6,10 @@ export default function Home() {
   const [data, setData] = useState<{ status?: string } | null>(null);
 
   useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api`)
+    const baseUrl =
+      process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+
+    fetch(`${baseUrl}/api`)
       .then((res) => res.json())
       .then((data) => setData(data))
       .catch(() => setData({ status: "Backend not reachable" }));
